5f4d022892435ab3ed2f0a71bc2738fcd57b08a7
unknown <egor767680@gmail.com>
2022-11-11 09:09:55 +0100

    ver1

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ import os
 
 VERSION = '0.0.1'
 DESCRIPTION = 'A library with package'
-LONG_DESCRIPTION = 'A library with package. Long disc'
+LONG_DESCRIPTION = 'file: README.md'
 # Setting up
 setup(
-    name="Library_python",
+    name="taskpackagelib",
     version=VERSION,
-    author="ThMeyerNeu (Meferko)",
+    author="ThMeyerNeu",
     author_email="<egor767680@gmial.com>",
     description=DESCRIPTION,
-    packages=find_packages("package1, package2, package3"),
+    packages=find_packages(),
     install_requires=[],
     keywords=['python'],
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Operating System :: Linux",
+        "Operating System :: Unix",
         "Operating System :: Microsoft :: Windows",
     ]
 )
