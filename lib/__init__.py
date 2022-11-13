30997fa3b783b3ba92bab308f7279484ba539d6d
ThMeyerNeu <egor767680@gmail.com>
2022-11-13 15:59:23 +0100

    fix2 ver17

---
+++ lib/__init__.py
@@ -0,0 +1,12 @@
+from .Task1 import *
+from .Task2 import *
+from .Task3_a import *
+from .Task3_b import *
+from .Task4 import *
+from .Task5 import *
+from .Task6 import *
+from .Task7_a import *
+from .Task7_b import *
+from .Task9 import *
+from .Task10 import *
+
